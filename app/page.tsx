dbfa8c09cc0448f6e89db792a459b6b3d4aeed11
Nelson Villarreal <devnelsonvillarreal@gmail.com>
2025-10-18 16:21:54 -0400

    feat: Adiciona section financial_services

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import Carousel from "./components/carousel";
+import ServicesHeroGrid from "./components/financial-services";
 
 export default function Home() {
   return (
@@ -32,6 +33,8 @@ export default function Home() {
           </div>
         </div>
       </section>
+
+      <ServicesHeroGrid />
 
       <section className="py-20">
         <div className="max-w-5xl mx-auto px-6 text-center">
