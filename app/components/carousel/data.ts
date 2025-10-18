d05e478257cd65c450e96bfe9cd268509f9ee238
Nelson Villarreal <devnelsonvillarreal@gmail.com>
2025-10-18 14:56:18 -0400

    fix: corrige local das images

--- app/components/carousel/data.ts
+++ app/components/carousel/data.ts
@@ -1,5 +1,5 @@
-import banner1 from '../../assets/images/banner01-1920-1080.png';
-import banner2 from '../../assets/images/banner02-1920-1080.png';
+import banner1 from '../../assets/carrossel/banner01-1920-1080.png';
+import banner2 from '../../assets/carrossel/banner02-1920-1080.png';
 
 export { banner1, banner2 };
 
